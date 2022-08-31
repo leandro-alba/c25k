899fa902ea34867fc77e3c00d60a65e88107a5be
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-31 17:45:34 -0400

    changes

--- routes/week5s2.js
+++ routes/week5s2.js
@@ -4,7 +4,7 @@ const week52Controller = require('../controllers/week5s2')
 
 router.get('/', week52Controller.getWeek5)
 
-router.post('/createWeek5', week52Controller.createWeek5)
+router.post('/createWeek5', week52Controller.createWeek5)//this is our POST method
 
 router.delete('/deleteWeek5', week52Controller.deleteWeek5)
 
