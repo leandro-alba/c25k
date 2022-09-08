e6d69f6a934d325dde98923aa6adae3bc65467fe
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-09-07 20:36:21 -0400

    changed css style for week1 page and week2 page

--- public/js/week1.js
+++ public/js/week1.js
@@ -1,4 +1,4 @@
-document.querySelector('.task1').addEventListener('touchstart',markComplete1)
+document.querySelector('.task1').addEventListener('click',markComplete1)
 document.querySelector('.task2').addEventListener('click',markComplete2)
 document.querySelector('.task3').addEventListener('click',t3)
 document.querySelector('.task4').addEventListener('click',t4)
@@ -44,89 +44,89 @@ async function deleteWeek1(){
 }
 
 function markComplete1(){
-    document.querySelector('.task1').classList.toggle('bbar')
+    document.querySelector('.task1').classList.toggle('bb')
 }
 function markComplete2(){
-    document.querySelector('.task2').classList.toggle('bbar')
+    document.querySelector('.task2').classList.toggle('bb')
 }
 function t3(){
-    document.querySelector('.task3').classList.toggle('bbar')
+    document.querySelector('.task3').classList.toggle('bb')
 }
 function t4(){
-    document.querySelector('.task4').classList.toggle('bbar')
+    document.querySelector('.task4').classList.toggle('bb')
 }
 function t5(){
-    document.querySelector('.task5').classList.toggle('bbar')
+    document.querySelector('.task5').classList.toggle('bb')
 }
 function t6(){
-    document.querySelector('.task6').classList.toggle('bbar')
+    document.querySelector('.task6').classList.toggle('bb')
 }
 function t7(){
-    document.querySelector('.task7').classList.toggle('bbar')
+    document.querySelector('.task7').classList.toggle('bb')
 }
 function t8(){
-    document.querySelector('.task8').classList.toggle('bbar')
+    document.querySelector('.task8').classList.toggle('bb')
 }
 function t9(){
-    document.querySelector('.task9').classList.toggle('bbar')
+    document.querySelector('.task9').classList.toggle('bb')
 }
 function t10(){
-    document.querySelector('.task10').classList.toggle('bbar')
+    document.querySelector('.task10').classList.toggle('bb')
 }
 function t11(){
-    document.querySelector('.task11').classList.toggle('bbar')
+    document.querySelector('.task11').classList.toggle('bb')
 }
 function t12(){
-    document.querySelector('.task12').classList.toggle('bbar')
+    document.querySelector('.task12').classList.toggle('bb')
 }
 function t13(){
-    document.querySelector('.task13').classList.toggle('bbar')
+    document.querySelector('.task13').classList.toggle('bb')
 }
 function t14(){
-    document.querySelector('.task14').classList.toggle('bbar')
+    document.querySelector('.task14').classList.toggle('bb')
 }
 function t15(){
-    document.querySelector('.task15').classList.toggle('bbar')
+    document.querySelector('.task15').classList.toggle('bb')
 }
 function t16(){
-    document.querySelector('.task16').classList.toggle('bbar')
+    document.querySelector('.task16').classList.toggle('bb')
 }
 function t17(){
-    document.querySelector('.task17').classList.toggle('bbar')
+    document.querySelector('.task17').classList.toggle('bb')
 }
 function t18(){
-    document.querySelector('.task18').classList.toggle('bbar')
+    document.querySelector('.task18').classList.toggle('bb')
 }
 
 // document.querySelector('.task19').addEventListener('touchstart', e=>{
-//     document.querySelector('.task19').classList.toggle('bbar')
+//     document.querySelector('.task19').classList.toggle('bb')
 // })
 
-const colorBtn = document.querySelector('.color')
-const mdn = document.querySelector('.mdn')
-const stylesheet = document.styleSheets[0];
-const mdnParaRule = [...stylesheet.cssRules].find((r)=> r.selectorText === '.mdn span')
-colorBtn.addEventListener('click', setRandColor)
-function setRandColor(){
+// const colorBtn = document.querySelector('.color')
+// const mdn = document.querySelector('.mdn')
+// const stylesheet = document.styleSheets[0];
+// const mdnParaRule = [...stylesheet.cssRules].find((r)=> r.selectorText === '.mdn span')
+// colorBtn.addEventListener('click', setRandColor)
+// function setRandColor(){
     //const newColor = 
-    mdnParaRule.style.setProperty('color', 'white')
-}
-const undoBtn = document.querySelector('.undo')
-undoBtn.addEventListener('click', setOldColor)
-function setOldColor(){
-    mdnParaRule.style.setProperty('color','black')
-}
+//     mdnParaRule.style.setProperty('color', 'white')
+// }
+// const undoBtn = document.querySelector('.undo')
+// undoBtn.addEventListener('click', setOldColor)
+// function setOldColor(){
+//     mdnParaRule.style.setProperty('color','black')
+// }
 
-const check = document.querySelector('.check')
-const mdn1 = document.querySelector('.mdn1')
-const mdnParaRule1 = [...stylesheet.cssRules].find((r)=> r.selectorText === '.mdn1 span')
-check.addEventListener('click', setCheck)
-function setCheck(){
+// const check = document.querySelector('.check')
+// const mdn1 = document.querySelector('.mdn1')
+// const mdnParaRule1 = [...stylesheet.cssRules].find((r)=> r.selectorText === '.mdn1 span')
+// check.addEventListener('click', setCheck)
+// function setCheck(){
     //const newColor = 
-    mdnParaRule1.style.setProperty('text-decoration','line-through black')//text-decoration line-through does not work iphone.
-}
-const undoBtn1 = document.querySelector('.undo1')
-undoBtn1.addEventListener('click', setOldColor1)
-function setOldColor1(){
-    mdnParaRule1.style.setProperty('text-decoration','none')
-}
+    //mdnParaRule1.style.setProperty('text-decoration','line-through black')//text-decoration line-through does not work iphone.
+//}
+// const undoBtn1 = document.querySelector('.undo1')
+// undoBtn1.addEventListener('click', setOldColor1)
+// function setOldColor1(){
+//     mdnParaRule1.style.setProperty('text-decoration','none')
+// }
